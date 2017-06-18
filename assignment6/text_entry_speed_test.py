8d0363e2da19b6744d57e92b8a19842ebd710315
Fabian Schatz <Fabian.Schatz@googlemail.com>
2017-06-18 15:33:21 +0200

    added some comments

--- assignment6/text_entry_speed_test.py
+++ assignment6/text_entry_speed_test.py
@@ -97,20 +97,27 @@ class Test(QtWidgets.QWidget):
         self.text_edit.set_reference(self)
         self.sentence_display = QtWidgets.QLabel()
         self.initUI()
+        # staring with first sentence
         self.next_sentence()
 
     def initUI(self):
         self.setGeometry(300, 300, self.WIDTH, self.HEIGHT)
         self.setWindowTitle('Text Entry Test')
 
+        # initializing layout
         self.vlayout = QtWidgets.QVBoxLayout()
+        # setting layout of widget
         self.setLayout(self.vlayout)
 
+        # adding widgets to the layout
         self.vlayout.addWidget(self.sentence_display)
         self.vlayout.addWidget(self.text_edit)
         self.show()
 
     def read_sentences_from_file(self):
+        """
+        reading sentences from file
+        """
         sentences = []
         try:
             filepath = 'phrases2.txt'
@@ -124,6 +131,11 @@ class Test(QtWidgets.QWidget):
         return sentences
 
     def next_sentence(self):
+        """
+        increasing current task number
+        setting up all necessary variables
+        setting display and cleaning the textedit
+        """
         self.current_task_number += 1
         if self.current_task_number < len(self.sentences):
             next_sentence = self.sentences[self.current_task_number]
@@ -177,8 +189,6 @@ class TextEdit(QtWidgets.QTextEdit):
 
     def __init__(self):
         super(TextEdit, self).__init__()
-        # self.example = 'Text Edit Sample Text'
-        # self.setText(self.example)
         self.current_word = ''
         self.current_sentence = ''
         self.completer = None
@@ -194,6 +204,7 @@ class TextEdit(QtWidgets.QTextEdit):
         completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
 
         self.completer = completer
+        # connecting method
         self.completer.insertText.connect(self.insertCompletion)
 
     def insertCompletion(self, completion):
@@ -211,9 +222,9 @@ class TextEdit(QtWidgets.QTextEdit):
         QtWidgets.QTextEdit.focusInEvent(self, event)
 
     def keyPressEvent(self, ev):
-        if self.completer:
+        if self.completer: # checking if autocompletion is turned on
             tc = self.textCursor()
-
+            # checking if tab or return is pressed while the popup is visible
             if (ev.key() == QtCore.Qt.Key_Tab or ev.key() == QtCore.Qt.Key_Return) and self.completer.popup().isVisible():
                 self.completer.insertText.emit(self.completer.getSelected())
                 self.completer.setCompletionMode(QtWidgets.QCompleter.PopupCompletion)
@@ -225,7 +236,7 @@ class TextEdit(QtWidgets.QTextEdit):
 
             tc.select(QtGui.QTextCursor.WordUnderCursor)
             cr = self.cursorRect()
-
+            # checking if somthing is written
             if len(tc.selectedText()) > 0:
                 self.completer.setCompletionPrefix(tc.selectedText())
                 popup = self.completer.popup()
@@ -239,11 +250,12 @@ class TextEdit(QtWidgets.QTextEdit):
         else:
             super(TextEdit, self).keyPressEvent(ev)
 
+        # starting timers if they are not running
         if not self.test.sentence_timer_running:
             self.test.start_sentence_timer()
         if not self.test.word_timer_running:
             self.test.start_word_timer()
-
+        # logging
         if ev.key() == QtCore.Qt.Key_Return:
             self.log('key_pressed', event_value='enter')
         elif ev.key() == QtCore.Qt.Key_Tab:
@@ -313,6 +325,7 @@ class TextEdit(QtWidgets.QTextEdit):
 
 def main():
     auto_complete = False
+    # checking if autocompleting is turned on via commandline parameter
     if len(sys.argv) > 1 and sys.argv[1] == 'on':
         auto_complete = True
 
